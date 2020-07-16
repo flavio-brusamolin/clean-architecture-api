ddb3fcb39f153f49aac862d488c84437829fbb1d
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-16 15:31:04 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -0,0 +1,31 @@
+import { AddAccountModel } from '../../../../domain/use-cases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account-mongo-repository'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const accountData: AddAccountModel = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(accountData.name)
+    expect(account.email).toBe(accountData.email)
+    expect(account.password).toBe(accountData.password)
+  })
+})
