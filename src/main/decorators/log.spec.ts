6759bf7db3e9580f4b7dbe6f4c433f7d4b7ceea8
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-17 14:59:55 -0300

    refactor: move sut creation to a factory function

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,24 +1,61 @@
 import { LogControllerDecorator } from './log'
 import { HttpRequest, Controller, HttpResponse } from '../../presentation/protocols'
 
-class ControllerStub implements Controller {
-  public async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return {
-      statusCode: 200,
-      body: {
-        id: 'any_id',
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'hashed_password'
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    public async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return {
+        statusCode: 200,
+        body: {
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'hashed_password'
+        }
       }
     }
+  }
+
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
   }
 }
 
 describe('Log Controller Decorator', () => {
   test('Should call controller handle', async () => {
-    const controllerStub = new ControllerStub()
-    const sut = new LogControllerDecorator(controllerStub)
+    const { sut, controllerStub } = makeSut()
+
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(handleSpy).toBeCalledWith(httpRequest)
+  })
+
+  test('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
 
     const handleSpy = jest.spyOn(controllerStub, 'handle')
 
