4618cdeb2d4b2716650ff58b03b6e45fe80c4e24
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-20 16:21:25 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,20 @@
+import { LoginController } from './login'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
+import { HttpRequest } from '../../protocols'
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
