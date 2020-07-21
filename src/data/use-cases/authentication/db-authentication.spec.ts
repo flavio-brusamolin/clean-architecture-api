be12719ff8ce19024fca9714a778a85d71d44a09
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-21 14:46:45 -0300

    test: ensure DbAuthentication throws if LoadAccountByEmailRepository throws

--- src/data/use-cases/authentication/db-authentication.spec.ts
+++ src/data/use-cases/authentication/db-authentication.spec.ts
@@ -50,4 +50,16 @@ describe('DbAuthentication Use Case', () => {
 
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
+
+  test('Should throw if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.auth(makeFakeAuthentication())
+
+    await expect(promise).rejects.toThrow()
+  })
 })
