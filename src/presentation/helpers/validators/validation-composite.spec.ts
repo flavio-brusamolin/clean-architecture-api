bbb337e5848c6842760f2be8a6fbb502b0bd698b
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-21 11:36:13 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -52,4 +52,14 @@ describe('Validation Composite', () => {
 
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({
+      name: 'any_name'
+    })
+
+    expect(error).toBeFalsy()
+  })
 })
