b843e591179a40a3c82fd6121f69ba4d3cc1df01
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-21 11:11:37 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -11,4 +11,14 @@ describe('RequiredField Validation', () => {
 
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+
+    const error = sut.validate({
+      field: 'any_value'
+    })
+
+    expect(error).toBeFalsy()
+  })
 })
