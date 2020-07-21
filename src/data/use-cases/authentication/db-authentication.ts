61dc17158a3d3c8bce8340bef59301b60199c722
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-21 15:53:27 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -1,25 +1,29 @@
 import { Authentication, AuthenticationModel } from '../../../domain/use-cases/authentication'
-import { LoadAccountByEmailRepository, HashComparer, TokenGenerator } from '../../protocols'
+import { LoadAccountByEmailRepository, HashComparer, TokenGenerator, UpdateAccessTokenRepository } from '../../protocols'
 
 export class DbAuthentication implements Authentication {
   public constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly tokenGenerator: TokenGenerator) {
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository) {
   }
 
   public async auth (authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
-
-    if (account) {
-      const match = await this.hashComparer.compare(authentication.password, account.password)
-
-      if (match) {
-        const accessToken = await this.tokenGenerator.generate(account.id)
-        return accessToken
-      }
+    if (!account) {
+      return null
     }
 
-    return null
+    const match = await this.hashComparer.compare(authentication.password, account.password)
+    if (!match) {
+      return null
+    }
+
+    const accessToken = await this.tokenGenerator.generate(account.id)
+
+    await this.updateAccessTokenRepository.update(account.id, accessToken)
+
+    return accessToken
   }
 }
