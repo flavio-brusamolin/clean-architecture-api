91df7992b725dd8c5a35da4486e1e72761931448
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-21 11:25:41 -0300

    test: ensure ValidationComposite return an error if any validation fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,22 @@
+import { ValidationComposite } from './validation-composite'
+import { MissingParamError } from '../../errors'
+import { Validation } from './validation'
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    class ValidationStub implements Validation {
+      public validate (input: any): Error {
+        return new MissingParamError('field')
+      }
+    }
+
+    const validationStub = new ValidationStub()
+    const sut = new ValidationComposite([validationStub])
+
+    const error = sut.validate({
+      field: 'any_value'
+    })
+
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
