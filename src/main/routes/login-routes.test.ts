99c0d05ddb8b47ceef997823aff087c0e1afed56
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-22 14:33:21 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'test@mail.com',
+          password: 'test123'
+        })
+        .expect(401)
+    })
   })
 })
