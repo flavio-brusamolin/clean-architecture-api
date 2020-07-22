ed4328aaadac3c90f69539ad31e39aa6c7f6f48f
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-22 09:56:52 -0300

    test: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapter } from './jwt-adapter'
+
+describe('Jwt Adapter', () => {
+  test('Should call sign with correct values', async () => {
+    const secret = 'secret'
+    const sut = new JwtAdapter(secret)
+
+    const signSpy = jest.spyOn(jwt, 'sign')
+
+    await sut.encrypt('any_id')
+
+    expect(signSpy).toHaveBeenCalledWith({
+      id: 'any_id'
+    }, 'secret')
+  })
+})
