1944c8ba095a8b0765c751b1b838eb5581926b55
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-22 14:17:15 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-architecture-api',
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 8080,
+  jwtSecret: process.env.JWT_SECRET || 'tK9$c12!a&&j0P'
 }
