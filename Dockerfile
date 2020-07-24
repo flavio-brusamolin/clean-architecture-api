2be133acfcdba33881b4a8e7759d1847463cda0f
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-24 18:24:12 +0000

    chore: add docker config for development

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,6 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 
-COPY . .
+COPY src ./src
 
-ARG NODE_ENV=production
-RUN npm run build
-
-EXPOSE 8080
-CMD [ "npm", "start" ]
+CMD [ "npm", "run", "dev" ]
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-architecture-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-architecture',
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || 'tK9$c12!a&&j0P'
 }
