f264451b062b6a6d06d7284f3eb6c4dabd888a78
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-07-24 19:31:05 +0000

    refactor: remove unnecessary 'await'

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -5,7 +5,6 @@ export class JwtAdapter implements Encrypter {
   public constructor (private readonly secret: string) {}
 
   public async encrypt (value: string): Promise<string> {
-    const accessToken = await jwt.sign({ id: value }, this.secret)
-    return accessToken
+    return jwt.sign({ id: value }, this.secret)
   }
 }
