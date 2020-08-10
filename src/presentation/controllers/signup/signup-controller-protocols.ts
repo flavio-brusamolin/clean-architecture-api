2e9b95b26f68b86b6df6a33d177496d5489f461a
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-10 13:03:34 +0000

    refactor: move validation to its own layer

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -1,5 +1,4 @@
 export * from '../../protocols'
-export * from '../../protocols/validation'
 export * from '../../../domain/models/account'
 export * from '../../../domain/use-cases/add-account'
 export * from '../../../domain/use-cases/authentication'
