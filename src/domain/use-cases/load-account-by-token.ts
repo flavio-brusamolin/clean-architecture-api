3193daae03fdf38d4a2c87cdd9f7e014f5f1adca
Flavio Brito <flavio.brito@sensedia.com>
2021-02-26 19:28:09 +0000

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct accessToken

---
+++ src/domain/use-cases/load-account-by-token.ts
@@ -0,0 +1,5 @@
+import { Account } from '../models/account'
+
+export interface LoadAccountByToken {
+  load: (accessToken: string, role?: string) => Promise<Account>
+}
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,17 @@
+import { LoadAccountByToken } from '../../domain/use-cases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
-  public async handle (_httpRequest: HttpRequest): Promise<HttpResponse> {
+  public constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
+
+  public async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.headers?.['x-access-token']
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken)
+    }
+
     return forbidden(new AccessDeniedError())
   }
 }
