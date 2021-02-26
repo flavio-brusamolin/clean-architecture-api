ef2e8f1c679289220233913b3a184d467311ff94
Flavio Brito <flavio.brito@sensedia.com>
2021-02-26 20:00:58 +0000

    feat: ensure AuthMiddleware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,22 +1,26 @@
 import { LoadAccountByToken } from '../../domain/use-cases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
-import { forbidden, ok } from '../helpers/http/http-helper'
+import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
   public constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
 
   public async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
 
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken)
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken)
 
-      if (account) {
-        return ok({ accountId: account.id })
+        if (account) {
+          return ok({ accountId: account.id })
+        }
       }
-    }
 
-    return forbidden(new AccessDeniedError())
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
