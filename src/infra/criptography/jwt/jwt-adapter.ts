ac905fa2d8bf36003ccd1ffd36eb1b1ac4b22710
Flavio Brito <flavio.brito@sensedia.com>
2021-03-09 19:36:53 +0000

    feat: ensure JwtAdapter calls verify with correct values

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -1,10 +1,16 @@
 import jwt from 'jsonwebtoken'
 import { Encrypter } from '../../../data/protocols'
+import { Decrypter } from '../../../data/protocols/criptography/decrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   public constructor (private readonly secret: string) {}
 
   public async encrypt (value: string): Promise<string> {
     return jwt.sign({ id: value }, this.secret)
+  }
+
+  public async decrypt (value: string): Promise<string> {
+    jwt.verify(value, this.secret)
+    return null
   }
 }
